f4d481e0429b6f9c83bafdaae1207e50caa401a0
cch1 <cch1@aff2ea43-9cdb-4955-b5a5-e63d6d2749c7>
2007-02-11 02:00:15 +0000

    Included authentication test helper methods in Test::Unit::TestCase
    git-svn-id: svn://cho.hapgoods.com/wcdw/authenticate/trunk@6 aff2ea43-9cdb-4955-b5a5-e63d6d2749c7

--- init.rb
+++ init.rb
@@ -1,5 +1,7 @@
 # Load authentication libs
 require "authenticated_system"
 require "authenticated_user"
-ActionController::Base.send(:include, Authentication::AuthenticatedSystem)
+require 'authenticated_test_helper'
+ActionController::Base.send :include, Authentication::AuthenticatedSystem
 ActiveRecord::Base.extend Authentication::AuthenticatedUser::ClassMethods
+Test::Unit::TestCase.send :include, Authentication::AuthenticatedTestHelper
