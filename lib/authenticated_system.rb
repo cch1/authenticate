98bd4643bb044c3855fbd017e0c09e36cc00327d
cch1 <cch1@aff2ea43-9cdb-4955-b5a5-e63d6d2749c7>
2007-08-03 16:26:54 +0000

    Bump authentication token on any subsequent authentication (like session). More verbose logging of authentication and login. Set authentication method instance variable only on successful authentication. Store authentication method in session only on initial login.
    git-svn-id: svn://cho.hapgoods.com/wcdw/authenticate/trunk@35 aff2ea43-9cdb-4955-b5a5-e63d6d2749c7

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -10,15 +10,22 @@ module Authenticate
     
     # Assigns the current_user, effectively performing login and logout operations.
     def current_user=(u)
+      return if @current_user == u
       @current_user = u
       User.current = u
       if u
+        if cookies[:authentication_token]
+          u.bump_token_expiry # Could regenerate token instead for nonce behavior
+          cookies[:authentication_token] = { :value => u.security_token , :expires => u.token_expiry }
+        end
+        session[:authentication_method] = @authentication_method # Record authentication method used at login.
         session[:user] = u.id
-        session[:authentication_method] ||= @authentication_method
+        logger.info "Authentication: User #{u.login} logged in via #{session[:authentication_method]} and authenticated via #{@authentication_method}."
       else # remove persistence
         cookies.delete :authentication_token
         session[:user] = nil
         session[:authentication_method] = nil
+        logger.info "User logged out."
       end
     end
     
@@ -73,40 +80,48 @@ module Authenticate
     
     # Identifies the authenticated user, if any.
     def authenticated_user
-      u = user_by_session_cookie || user_by_authentication_cookie || user_by_http_auth || user_by_token
-      @authentication_method = nil unless u
-      u
+      user_by_session_cookie || user_by_authentication_cookie || user_by_http_auth || user_by_token
     end
 
     # Attempt to authenticate with a URL-encoded security token
     def user_by_token
-      @authentication_method = :token
-      User.authenticate_by_token(params[:security_token])
+      logger.debug "Authentication: attempting to authenticate via token."
+      if (user = User.authenticate_by_token(params[:security_token]))
+        @authentication_method = :token
+        logger.debug "Authentication: authenticated #{user.login} via token."
+      end
+      user
     end
     
     # Attempt to authenticate with a cookie-based security token
     def user_by_authentication_cookie
-      @authentication_method = :cookie
+      logger.debug "Authentication: attempting to authenticate via authentication cookie."
       if (user = User.authenticate_by_token(cookies[:authentication_token]))
-        user.bump_token_expiry # Could regenerate token instead for nonce behavior
-        cookies[:authentication_token] = { :value => user.security_token , :expires => user.token_expiry }
+        @authentication_method = :cookie
+        logger.debug "Authentication: authenticated #{user.login} via authentication cookie."
       end
-      return user
+      user
     end
     
     # Attempt to authenticate with HTTP Auth information
     def user_by_http_auth
-      @authentication_method = :http_authentication
-      user, password = get_http_auth_data
-      if user
-        User.authenticate(user, password)
+      logger.debug "Authentication: attempting to authenticate via HTTP Auth."
+      login, password = get_http_auth_data
+      if (user = login && User.authenticate(login, password))
+        @authentication_method = :http_authentication        
+        logger.debug "Authentication: authenticated #{user.login} via HTTP Auth."
       end
+      user
     end
   
     # Attempt to authenticate with session cookie
     def user_by_session_cookie
-      @authentication_method = :session # This should never be an *initial* authentication method.
-      session[:user] && User.find(session[:user])
+      logger.debug "Authentication: attempting to authenticate via session."
+      if (user = session[:user] && User.find(session[:user]))
+        @authentication_method = :session # This should never be an *initial* authentication method.        
+        logger.debug "Authentication: authenticated #{user.login} via session cookie."
+      end
+      user
     end
   
     private
