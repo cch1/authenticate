f18365d6eb6bfbb04b20db8c516ce7052cc6f16d
cch1 <cch1@aff2ea43-9cdb-4955-b5a5-e63d6d2749c7>
2007-11-09 23:12:03 +0000

    Changed load namespacing to avoid collisions with exceptions.rb.
    git-svn-id: svn://cho.hapgoods.com/wcdw/authenticate/trunk@45 aff2ea43-9cdb-4955-b5a5-e63d6d2749c7

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 require 'authenticated_system'
 require 'authenticated_user'
 require 'authenticated_test_helper'
-require 'exceptions'
+require 'GroupSmarts/authenticate/exceptions'
 ActionController::Base.send :include, Authenticate::AuthenticatedSystem
 ActiveRecord::Base.extend Authenticate::AuthenticatedUser::ClassMethods
 Test::Unit::TestCase.send :include, Authenticate::AuthenticatedTestHelper
