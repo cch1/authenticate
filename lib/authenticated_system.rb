fcea2a45e8cc527bdcc92fb123b1984296a7d7d9
cch1 <cch1@aff2ea43-9cdb-4955-b5a5-e63d6d2749c7>
2007-12-20 22:32:12 +0000

    Refactored HTTP Basic Authentication to use Rails' recently added support.
    git-svn-id: svn://cho.hapgoods.com/wcdw/authenticate/trunk@47 aff2ea43-9cdb-4955-b5a5-e63d6d2749c7

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -106,10 +106,8 @@ module Authenticate
     # Attempt to authenticate with HTTP Auth information
     def user_by_http_auth
       logger.debug "Authentication: attempting to authenticate via HTTP Auth."
-      login, password = get_http_auth_data
-      if (user = login && User.authenticate(login, password))
-        @authentication_method = :http_authentication        
-        logger.debug "Authentication: authenticated #{user.login} via HTTP Auth."
+      if user = authenticate_with_http_basic { |uid, pwd| User.authenticate(uid, pwd) }
+        @authentication_method = :http_authentication
       end
       user
     end
@@ -123,15 +121,5 @@ module Authenticate
       end
       user
     end
-  
-    private
-    @@http_auth_headers = %w(X-HTTP_AUTHORIZATION HTTP_AUTHORIZATION Authorization)
-    # gets HTTP Authentication info
-    def get_http_auth_data
-      auth_key  = @@http_auth_headers.detect { |h| request.env.has_key?(h) }
-      auth_data = request.env[auth_key].to_s.split unless auth_key.blank?
-      return auth_data && auth_data[0] == 'Basic' ? Base64.decode64(auth_data[1]).split(':')[0..1] : [nil, nil] 
-    end
-    
   end
 end
